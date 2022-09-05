19ad310aee70e96047d4dbf87d693186836bb7d5
Titus Mumo <titusmumo@gmail.com>
2022-09-05 20:51:15 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+function Scooter(year, color, model){
+   this.year = year
+   this.color = color
+   this.model = model
+}
+const samson = new Scooter(2015, 'red', 'swoosh') 
+
+function Driver(name, age, experience){
+    this.name = name
+    this.age = age
+    this.experience = experience
+}
+
+function PickupLocation(address, city){
+    this.address = address
+    this.city = city
+}
